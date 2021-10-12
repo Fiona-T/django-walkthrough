837539cda0541fae8fb2b91541ab14428e019717
Fiona-T <fitracey@gmail.com>
2021-10-12 14:44:39 +0000

    add button on todo_list.html to create new item, links to new add_item.html template, update views with new function and urls.py with new url path

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 # Create your views here.
@@ -12,3 +12,17 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    # if POST request, means it is Add New Item form
+    if request.method == 'POST':
+        # two new variables to hold info from form
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        # create new db record using info in variables above
+        Item.objects.create(name=name, done=done)
+        # redirect user to home page
+        return redirect('get_todo_list')
+    # if not a POST request, then just get the page and render it
+    return render(request, 'todo/add_item.html')
