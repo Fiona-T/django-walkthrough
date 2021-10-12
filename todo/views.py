69985c1edd58da5303a40951bee12caafbbf1d3f
Fiona-T <fitracey@gmail.com>
2021-10-12 13:46:15 +0000

    update views.py to retrieve todo items from db, and todo_list.html to display them

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .models import Item
 
 # Create your views here.
+
+
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    # get query set of all items from db
+    items = Item.objects.all()
+    # dictionary of all the items, passed to render below
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
