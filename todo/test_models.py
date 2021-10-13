b351e08a5a1a0e9e74f270f4ef1c093b39b512be
Fiona-T <fitracey@gmail.com>
2021-10-13 18:02:57 +0000

    add tests in test_models.py to check done status defaults to false

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
+from .models import Item
 
 # Create your tests here.
 
 
-class TestDjango(TestCase):
-    # pass
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+class TestModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        # create a test item with just name
+        item = Item.objects.create(name='Test item')
+        # done status of the item should be false, as it was not set
+        self.assertFalse(item.done)
