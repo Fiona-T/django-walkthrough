b28c70b4ffa9cadbfc1d392bf379bf0f508dc167
Fiona-T <fitracey@gmail.com>
2021-10-13 10:15:03 +0000

    update todo_list.html with Toggle button, update urls.py and views.py accordingly

--- todo/views.py
+++ todo/views.py
@@ -58,3 +58,12 @@ def edit_item(request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    # get a copy of the item from db
+    item = get_object_or_404(Item, id=item_id)
+    # update the item's done status to the opposite of what it was
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
